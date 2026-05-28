b36d58a1b03a3ed52895f8877af927ac6b78272e
kushal <dahalkushal2212@gmail.com>
2026-05-28 10:09:15 +0545

    small fix

--- 09Project/src/App.jsx
+++ 09Project/src/App.jsx
@@ -1,15 +1,18 @@
-import {  } from 'react'
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Porfile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
- 
+  
 
   return (
-    <>
-    <h1>Hello, this is context api session</h1>
-   
-    </>
+    <UserContextProvider>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
--- 09Project/src/components/Login.jsx
+++ 09Project/src/components/Login.jsx
@@ -1,33 +1,31 @@
-import UserContext from '../context/UserContextProvider'
-import { useState, useContext } from 'react'
+import  {useState, useContext} from 'react'
+import UserContext from '../context/UserContext'
 
-function Login(){
+function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
     const {setUser} = useContext(UserContext)
 
-    const handleLogin = (e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault()
         setUser({username, password})
     }
-
-    return(
-        <form onSubmit={handleLogin}>
-            <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit">Login</button>
-        </form>
-    )}
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type='text'
+        value={username}
+        onChange={(e) => setUsername(e.target.value) }
+        placeholder='username'  />
+        {" "}
+        <input type='text' 
+        value={password}
+        onChange={(e) => setPassword(e.target.value) }
+        placeholder='password'  />
+        <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
 
 export default Login
